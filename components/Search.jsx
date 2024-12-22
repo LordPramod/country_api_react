1948206cf46ec8f465eadf7f1ed0f24fe23ece3a
PRAMOD THAPA <thapapramod821@gmail.com>
2024-12-22 15:47:03 +0545

    initial commit

---
+++ components/Search.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Search() {
+  return (
+    <>
+    <div className="search-container">
+      <i className="fa-solid fa-magnifying-glass"></i>
+      <input type="text" placeholder="Search for a country..." />
+    </div>
+    </>
+  )
+}
+
+export default Search

bd851cb2f8e9daffcb2431554be499fe4298af48
PRAMOD THAPA <thapapramod821@gmail.com>
2024-12-23 09:03:59 +0545

    Search Implement

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import App from '../App'
+import { useState } from 'react-dom'
 
-function Search() {
+function Search({query, setQuery}) {
   return (
     <>
     <div className="search-container">
       <i className="fa-solid fa-magnifying-glass"></i>
-      <input type="text" placeholder="Search for a country..." />
+      <input onChange={(e) => 
+        // console.log(e.target.value)
+        setQuery(e.target.value)
+        } type="text" placeholder="Search for a country..." />
     </div>
     </>
   )
